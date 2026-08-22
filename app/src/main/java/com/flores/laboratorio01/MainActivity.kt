c7000894ffcd8fc029d07510436d24867f1034db
yoselin55 <yoselin.flores@tecsup.edu.pe>
2026-08-21 21:09:10 -0500

    Agrega saludo personalizado con nombre del alumno y boton del reto

--- app/src/main/java/com/flores/laboratorio01/MainActivity.kt
+++ app/src/main/java/com/flores/laboratorio01/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -43,6 +44,9 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         Text(
             text = "Curso: Programación en Móviles"
         )
+        Button(onClick = { /* Sin acción */ }) {
+            Text(text = "Presióname")
+        }
     }
 }
 
